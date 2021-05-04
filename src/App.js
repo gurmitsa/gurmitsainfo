4110df82558970fb5c72e315771767a23f973a4e
Gurmit Singh Arora <gurmitsa@gmail.com>
2021-05-03 21:02:59 -0400

    bootstrapping the app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Welcome to gurmitsa.info</code>
+        </p>
+        <a
+          className="App-link"
+          href="http://www.gurmitsingh.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn More
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
